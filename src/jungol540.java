3e50a9e1a8384abd03f1a8ec22476d7634e9253a
DKS2312 <dtg2312@gmail.com>
2022-06-16 09:40:25 +0900

    Create jungol540.java

---
+++ src/jungol540.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class jungol540 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		while(true) {
+			int num = scan.nextInt();
+			
+			if(num == -1) break;
+			if(num%3==0) System.out.println(num/3);
+		}
+	}
+}
