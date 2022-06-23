7a4569ead84f92229047785c5ba63cf7b994917b
DKS2312 <dtg2312@gmail.com>
2022-06-23 10:16:51 +0900

    Create jungol557.java

---
+++ src/jungol557.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class jungol557 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+//		String[] str = scan.nextLine().split(" ");
+//		System.out.println(str[0]+" "+str[3]+" "+str[6]);
+		
+		//String <-> char X
+//		char a = scan.next();	//char <- String
+		
+		
+		String str1 = new String("ABCD");
+		System.out.println(str1);
+		
+		scan.close();
+	}
+}
